ddaedac360f403884a9562175027afb387b8fc02
Disnasudheer <119562836+Disnasudheer@users.noreply.github.com>
2023-12-27 19:53:35 +0530

    Add files via upload

---
+++ disna/assignment2/countdwn.py
@@ -0,0 +1,7 @@
+num=int(input("Enter the number:"))
+count=num
+while(num>0):
+    print(count)
+    count-=1
+    if(count<=0):
+        break
---
+++ disna/assignment2/frtlist.py
@@ -0,0 +1,8 @@
+fruits = ["apple", "banana", "chikkoo", "dragonfruit", "grape"]
+newlist = []
+
+for x in fruits:
+  if "e" in x:
+    newlist.append(x)
+
+print(newlist)
