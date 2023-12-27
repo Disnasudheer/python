5ef65a44aa2b9544c094fa05aeb6a694fa730509
Disnasudheer <119562836+Disnasudheer@users.noreply.github.com>
2023-12-27 19:55:52 +0530

    Delete disna/largest.py

--- disna/largest.py
+++
@@ -1,9 +0,0 @@
-x=int(input(" "))
-y=int(input(" "))
-z=int(input(" "))
-if(x>y) and (x>z):
-    print(x,"is greater")
-elif(y>x) and (y>z):
-    print(y,"is greater")
-else:
-    print(z,"is greater")
